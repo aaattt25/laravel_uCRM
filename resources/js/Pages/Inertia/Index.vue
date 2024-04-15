8cc424f8823496988046caef82ca848d93f4486f
aaattt25 <p.aaattt25@gmail.com>
2024-04-15 14:49:47 +0900

    Vue3-sec2

---
+++ resources/js/Pages/Inertia/Index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'  // definePropsを書くときに書くのかな
+import { ref } from 'vue'     // definePropsを書くときに書くのかな
+
+defineProps({
+  blogs: Array
+})
+</script>
+
+<template>
+  <div v-if="$page.props.flash.message" class="bg-blue-200">
+    {{ $page.props.flash.message }}
+  </div>
+  <ul>
+    <li v-for="blog in blogs" :key="blog.id">
+      件名：<Link class="" :href="route('inertia.show', { id: blog.id })">ルートパラメーターのテストです{{ blog.title }}</Link><br>
+      本文：{{ blog.content }}
+    </li>
+  </ul>
+</template>
